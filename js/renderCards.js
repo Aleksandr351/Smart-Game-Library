77146754b10eef5d357dabe7e02044c3ec7c3840
Aleksandr351 <burdyko351@gmail.com>
2026-06-30 21:41:01 +0300

    mediaForPhone

--- js/renderCards.js
+++ js/renderCards.js
@@ -6,27 +6,35 @@ export let renderCards = function (gamesArray, gameList) {
 	article.classList.add('game-card');
 
 	const gameTitle = document.createElement('h3');
+	gameTitle.classList.add("game-card__title");
 	gameTitle.textContent = gamesArray[i].title;
 
 	const genre = document.createElement('p');
+	genre.classList.add("game-card__info");
 	genre.textContent = "Жанр: " + gamesArray[i].genre;
 
 	const year = document.createElement('p');
+	year.classList.add("game-card__info");
 	year.textContent = "Год: " + gamesArray[i].year;
 
 	const rating = document.createElement('p');
+	rating.classList.add("game-card__info");
 	rating.textContent = "Оценка: " + gamesArray[i].rating;
 
 	const platform = document.createElement("p");
+	platform.classList.add("game-card__info");
 	platform.textContent = 'Платформа: ' + gamesArray[i].platform;
 
 	const status = document.createElement("p");
+	status.classList.add("game-card__status");
 	status.textContent = 'Статус: ' + gamesArray[i].status;
 
 	const playTime = document.createElement("p");
+	playTime.classList.add("game-card__info");
 	playTime.textContent = "Время прохождения: " + gamesArray[i].playTime;
 
 	const description = document.createElement("p");
+	description.classList.add("game-card__description");
 	description.textContent = "Описание: " + gamesArray[i].description;
 
 	article.append(gameTitle);
