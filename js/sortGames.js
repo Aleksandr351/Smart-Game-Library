c06d8c8fabe35a38ceb97828e4532cd093fd65c5
Aleksandr351 <burdyko351@gmail.com>
2026-07-07 22:12:33 +0300

    refactor: simplify game sorting

--- js/sortGames.js
+++ js/sortGames.js
@@ -1,15 +1,10 @@
 export function sortGames (gamesArray, selectedSort) {
-	if (selectedSort === 'rating') {
-		gamesArray.sort(function(a, b) {
-			return b.rating - a.rating;
-		})
-	} else if (selectedSort === 'year') {
-		gamesArray.sort(function(a, b) {
-			return b.year - a.year;
-		})
-	} else if (selectedSort === 'playTime') {
-		gamesArray.sort(function(a, b){
-			return b.playTime - a.playTime;
-		})
+	if (selectedSort === 'default') {
+		return;
 	}
+
+	gamesArray.sort(function (a, b) {
+		return b[selectedSort] - a[selectedSort];
+	});
 }
+
