44f5d1018439c79c17b063f7fcfa57dc54586596
Aleksandr351 <burdyko351@gmail.com>
2026-07-08 00:07:34 +0300

    feat: reset filters

--- Smart-game-library/app.js
+++ Smart-game-library/app.js
@@ -8,6 +8,7 @@ const genreSelect = document.querySelector("#genreFilter");
 const statusSelect = document.querySelector("#statusFilter");
 const sortSelect = document.querySelector("#sortSelect");
 const gamesCount = document.querySelector('#gamesCount');
+const resetFilterButton = document.querySelector("#resetFilterButton");
 
 function updateGames() {
 	applyFilters(games, input, genreSelect, statusSelect,sortSelect, gamesCount, gameList);
@@ -17,7 +18,18 @@ input.addEventListener('input', 	updateGames);
 genreSelect.addEventListener('change', 	updateGames);
 statusSelect.addEventListener('change', 	updateGames);
 sortSelect.addEventListener('change', 	updateGames);
+resetFilterButton.addEventListener('click', resetFilters)
+
 
 renderSelectOptions(games, genreSelect, 'genre', 'Все жанры');
 renderSelectOptions(games, statusSelect, 'status', 'Все статусы');
 updateGames();
+
+function resetFilters () {
+	input.value = "";
+	genreSelect.value = 'all';
+	statusSelect.value = "all";
+	sortSelect.value = 'default';
+
+	updateGames();
+}
